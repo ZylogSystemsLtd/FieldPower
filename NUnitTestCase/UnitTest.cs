e715c8701bb336fa2009d4c88780b7b1b1b11a89
rajasrirangan <rajasrirangan@gmail.com>
2015-12-30 20:22:48 +0530

    Unit Test Case project Added

---
+++ NUnitTestCase/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace GitSample
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        MathsHelper helper = new MathsHelper();
+        [TestCase]
+        public void AddTest()
+        {
+            int result = helper.Add(20, 10);
+            Assert.AreEqual(40, result);
+        }
+        [TestCase]
+        public void SubtractTest()
+        {
+            int result = helper.Subtract(20, 10);
+            Assert.AreEqual(10, result);
+        }
+       
+    }
+}
